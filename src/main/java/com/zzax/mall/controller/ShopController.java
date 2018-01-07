ecb182fe8ae32bfdd897a2f609bd93891da531f7
llynic <llynic@163.com>
2018-01-07 21:19:24 +0800

    设置了访问页面

--- src/main/java/com/zzax/mall/controller/ShopController.java
+++ src/main/java/com/zzax/mall/controller/ShopController.java
@@ -1,6 +1,9 @@
 package com.zzax.mall.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -12,4 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/shop")
 public class ShopController {
+    public static final Logger logger = LoggerFactory.getLogger(ReceiptController.class);
+
+    @GetMapping("/myshop")
+    public String index(){
+        return "receipt/accessory";
+    }
 }
